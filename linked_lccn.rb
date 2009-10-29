fc36a0569073b701a718fa26f56c4ee311110dc8
Ross Singer <rossfsinger@gmail.com>
2009-10-29 15:08:16 -0400

    Added links to id.loc.gov

--- linked_lccn.rb
+++ linked_lccn.rb
@@ -4,6 +4,7 @@ require 'sinatra'
 require 'net/http'
 require 'enhanced_marc'
 require 'rdf_objects'
+require 'isbn/tools'
 include RDFObject
 
 configure do
@@ -42,6 +43,7 @@ def to_rdf(marc)
   when "MARC::SerialRecord" then model_serial(marc, resource)
   when "MARC::MapRecord" then model_map(marc, resource)
   when "MARC::VisualRecord" then model_visual(marc, resource)
+  else model_generic(marc, resource)
   end
   marc_common(resource, marc)
   resource
@@ -64,8 +66,56 @@ end
 def model_visual(marc, resource)
 end
 
+def model_generic(marc, resource)
+end
+
 def marc_common(resource, marc)
   resource.assert("[dcterms:title]", marc['245']['a'])
+  if marc['020'] && marc['020']['a']
+    isbn = ISBN_Tools.cleanup(marc['020']['a'].strip_trailing_punct)
+    if ISBN_Tools.is_valid?(isbn)
+      if isbn.length == 10
+        resource.assert("[bibo:isbn10]",isbn)
+        resource.relate("[owl:sameAs]", "http://purl.org/NET/book/isbn/#{isbn}#book")
+        resource.assert("[bibo:isbn13]", ISBN_Tools.isbn10_to_isbn13(isbn))
+      else
+        resource.assert("[bibo:isbn13]",isbn)
+        resource.assert("[bibo:isbn10]", ISBN_Tools.isbn13_to_isbn10(isbn))          
+        resource.relate("[owl:sameAs]", "http://purl.org/NET/book/isbn/#{ISBN_Tools.isbn13_to_isbn10(isbn)}#book")        
+      end
+    end
+  end
+  
+  if marc['022'] && marc['022']['a']
+    resource.assert("[bibo:issn]", marc['022']['a'].strip_trailing_punct)
+  end  
+  
+  subjects = marc.find_all {|field| field.tag =~ /^6../}
+  
+  subjects.each do | subject |
+    literal = subject_to_string(subject)
+    resource.assert("[dc:subject]", literal)
+    if !["653","690","691","696","697", "698", "699"].index(subject.tag) && subject.indicator2 =~ /^(0|1)$/      
+      
+      authority = get_lcsh(literal)
+      if authority
+        resource.relate("[dcterms:subject]", authority)
+      end
+    end  
+  end
+end
+
+def get_lcsh(subject_string)
+  u = URI.parse("http://id.loc.gov/authorities/label/#{subject_string.gsub(/\s/,"%20")}")
+  req = Net::HTTP::Get.new(u.path)
+  res = Net::HTTP.start(u.host, u.port) {|http|
+    http.request(req)
+  }
+  
+  if res.code == "302"
+    return res.header['location']
+  end
+  return nil
 end
 
 def to_rdfxml(resource)
@@ -101,3 +151,38 @@ def to_rdfxml(resource)
   rdf << "</rdf:Description></rdf:RDF>"
   rdf
 end
+
+def subject_to_string(subject)
+  literal = ''
+  subject.subfields.each do | subfield |
+    if !literal.empty?
+      if ["v","x","y","z"].index(subfield.code)
+        literal << '--'
+      else
+        literal << ' ' if subfield.value =~ /^[\w\d]/
+      end
+    end
+    literal << subfield.value
+  end
+  literal.strip_trailing_punct  
+end
+
+
+class String
+  def slug
+    slug = self.gsub(/[^A-z0-9\s\-]/,"")
+    slug.gsub!(/\s/,"_")
+    slug.downcase.strip_leading_and_trailing_punct
+  end  
+  def strip_trailing_punct
+    self.sub(/[\.:,;\/\s]\s*$/,'').strip
+  end
+  def strip_leading_and_trailing_punct
+    str = self.sub(/[\.:,;\/\s\)\]]\s*$/,'').strip
+    return str.strip.sub(/^\s*[\.:,;\/\s\(\[]/,'')
+  end  
+  def lpad(count=1)
+    "#{" " * count}#{self}"
+  end
+end
+
